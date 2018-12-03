351e31081462f6cb89fd9d8093fc56a5bff9caa6
Liam Stormbird <liam.stormbird@gmx.de>
2018-12-03 21:02:52 +0100

    Startbildschirm und erste Network Vorbereitung

---
+++ vr_schneeballschlacht/Assets/Scripts/Language/EnglishLanguage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.UI;
+
+public class EnglishLanguage
+{
+
+    public static void SetMainScreen(Text playButton, Text tutorialButton, Text optionButton, Text exitButton)
+    {
+        playButton.text = "Play";
+        tutorialButton.text = "Tutorial";
+        optionButton.text = "Options";
+        exitButton.text = "Quit Game";
+    }
+}
---
+++ vr_schneeballschlacht/Assets/Scripts/Language/GermanLanguage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.UI;
+
+public class GermanLanguage {
+
+    public static void SetMainScreen(Text playButton, Text tutorialButton, Text optionButton, Text exitButton) {
+        playButton.text = "Spielen";
+        tutorialButton.text = "Tutorial";
+        optionButton.text = "Optionen";
+        exitButton.text = "Spiel beenden";
+    }
+}
